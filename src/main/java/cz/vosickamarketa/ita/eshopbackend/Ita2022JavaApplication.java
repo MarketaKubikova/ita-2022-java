c6db2fecc7e71f2cf319bde95c70e9a90b5a2b1c
Markéta Vošická <vosickamarketa@gmail.com>
2023-01-26 17:46:04 +0100

    Initial commit

---
+++ src/main/java/cz/vosickamarketa/ita/eshopbackend/Ita2022JavaApplication.java
@@ -0,0 +1,13 @@
+package cz.vosickamarketa.ita.eshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ita2022JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ita2022JavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/vosickamarketa/ita/eshopbackend/Ita2022JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.vosickamarketa.ita.eshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ita2022JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

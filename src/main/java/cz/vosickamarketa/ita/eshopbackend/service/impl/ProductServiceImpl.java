79c6f1b4e33bcd3920df120218b937ffc567cc7c
Markéta Vošická <vosickamarketa@gmail.com>
2023-02-05 13:18:54 +0100

    added logging

--- src/main/java/cz/vosickamarketa/ita/eshopbackend/service/impl/ProductServiceImpl.java
+++ src/main/java/cz/vosickamarketa/ita/eshopbackend/service/impl/ProductServiceImpl.java
@@ -7,12 +7,14 @@ import cz.vosickamarketa.ita.eshopbackend.model.ProductDto;
 import cz.vosickamarketa.ita.eshopbackend.repository.ProductRepository;
 import cz.vosickamarketa.ita.eshopbackend.service.ProductService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@Slf4j
 @AllArgsConstructor
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -21,6 +23,8 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<ProductDto> getAllProducts() {
+        log.info("Returning all products in db.");
+
         return productRepository.findAll().stream()
                 .map(productMapper::toDto)
                 .toList();
@@ -28,9 +32,12 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductDto getProductById(Long id) {
-        return productRepository.findById(id)
+        ProductDto productDto = productRepository.findById(id)
                 .map(productMapper::toDto)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        log.info("Returning product with id %d.".formatted(id));
+
+        return productDto;
     }
 
     @Override
@@ -38,6 +45,7 @@ public class ProductServiceImpl implements ProductService {
         Product product = productMapper.toDomain(productDto);
 
         productRepository.save(product);
+        log.info("New product has been successfully saved to db with id %d.".formatted(product.getId()));
 
         return productMapper.toDto(product);
     }
@@ -49,6 +57,7 @@ public class ProductServiceImpl implements ProductService {
 
         productMapper.mergeProduct(product, productDto);
         productRepository.save(product);
+        log.info("Product with id %d has been successfully updated.".formatted(id));
 
         return productMapper.toDto(product);
     }
@@ -56,9 +65,10 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void deleteProduct(Long id) {
         if (!productRepository.existsById(id)) {
+            log.debug("Product with id %d cannot be found.".formatted(id));
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
-
+        log.info("Product with id %d has been deleted.".formatted(id));
         productRepository.deleteById(id);
     }
 }
